31cfb328a4c7ab06fa746e98e04d1919fefabb1a
bercivarga <65171545+bercivarga@users.noreply.github.com>
2023-11-29 21:17:00 +0100

    fix vertex

--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,10 +1,8 @@
 // basic vertex shader for threejs in glsl language
 
-varying vec3 vUv; 
+varying vec2 vUv;
 
 void main() {
-  vUv = position; 
-
-  vec4 modelViewPosition = modelViewMatrix * vec4(position, 1.0);
-  gl_Position = projectionMatrix * modelViewPosition; 
+  vUv = uv;
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
